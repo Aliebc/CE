2f01e2fdd9aa51441fa9a0cd630091893176ac94
Aliebc <aliebcx@outlook.com>
2022-02-06 20:58:34 +0800

    1.0.9

--- zyp/ce.py
+++ zyp/ce.py
@@ -1,7 +1,9 @@
+from email import header
 import os
 import time
 import json
 import math
+from wsgiref import headers
 import pandas as pd
 import numpy as npy
 from django.http import HttpResponse,JsonResponse
@@ -30,3 +32,9 @@ def ce_main(request):
 
 def ce_not_found(request):
     return JsonResponse(ret(-1,None,"Not Found"))
+
+def test(request):
+    df2 = pd.DataFrame({"Distance": [1,2,3], 'Force': [4,5.5,6], 'yerr': [0.1, 0.5, 3]})
+    df2.to_excel('/opt/rrr.xlsx')
+    rp=HttpResponse(open('/opt/rrr.xlsx','rb').read(),headers={'Content-Type':'application/octet-stream','Content-Disposition':'attachment;filename=test.xlsx'})
+    return rp
