4d941b55b1844323137023bf1839ea37a257c6cf
Aliebc <aliebcx@outlook.com>
2022-02-10 20:42:28 +0800

    1.0.17

--- zyp/ce.py
+++ zyp/ce.py
@@ -20,14 +20,9 @@ def ret(code,data,err):
     return {"respCode":code,"respData":data,"errMsg":err}
 
 def ce_main(request):
-    if(request.method!="POST"):
-        return JsonResponse(ret(-1,"","Only POST Method allowed."))
-    ip={"respCode":0,"respData":request.environ['HTTP_X_FORWARDED_FOR'],"errMsg":""}
-    ip=json.dumps(ip)
-    y=request.GET.get("rrr",default="No")
-    y=request.POST.get("rrr",default="No")
-    z=request.environ['HTTP_X_FORWARDED_FOR']
-    return JsonResponse(ret(0,request.environ['HTTP_X_FORWARDED_FOR'],None))
+    if(request.method not in ["POST","GET"]):
+        return JsonResponse(ret(-1,"","Bad Method"))
+    return JsonResponse(ret(0,{"api_name":"CE API version","version":"1.0.16"},None))
 
 def ce_not_found(request):
     return JsonResponse(ret(-1,None,"Not Found"))
