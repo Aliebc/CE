f7cf4ea732e22f90645911a9ca70a9ac191299f5
Aliebc <aliebcx@outlook.com>
2022-04-18 12:02:49 +0800

    1.4.10

---
+++ zyp/__init__.py
@@ -0,0 +1,8 @@
+"""
+# API for Computational Economics
+
+## Abstract
+
+This project provides HTTP interfaces for computing several commonly used analytical formulas in computational economics and data analysis, such as OLS or logit regression.
+
+"""
